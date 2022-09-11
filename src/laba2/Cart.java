461f5abf4790c1b3ef833960edf43a21ec27035a
Illya <svirillya@gmail.com>
2022-09-11 20:25:33 +0300

    added item and cart classes

--- src/laba2/Cart.java
+++ src/laba2/Cart.java
@@ -1,2 +1,65 @@
-package laba2;public class Cart {
+package laba2;
+/*
+добавить проверку на переполнение стека
+ */
+
+public class Cart {
+    //fields
+    private Item[] stack;       //stack of items
+    private int itemNumber;     //current number of items in the cart
+
+    //constructor; argument is maximum number of items in the stack
+    public Cart(int maxItemNumber) {
+        stack=new Item[maxItemNumber];
+        itemNumber=0;
+    }
+
+    //public methods
+
+    //adds an element to the end of the stack (cart)
+    public void addItem(Item item) {
+        stack[itemNumber]=item;
+        itemNumber++;
+    }
+
+    //removes the element at the end of the stack (cart)
+    public void deleteItem(Item item) {
+        stack[itemNumber-1]=null;
+        itemNumber--;
+    }
+
+    //calculates the total cost of items in the cart
+    public float sumItemPrice() {
+        int sum=0;          //sum of item prices
+        for (int i=0; i<stack.length; i++) {
+            sum+=stack[i].getPrice();
+        }
+        return sum;
+    }
+
+    //increase the price of all items in the cart by a certain percentage
+    public void increaseAllItemPrice(float percent) {
+        for (int i=0; i<stack.length; i++) {
+            stack[i].increasePrice(percent);
+        }
+    }
+
+    //decrease the price of all items in the cart by a certain percentage
+    public void decreaseAllItemPrice(float percent) {
+        for (int i=0; i<stack.length; i++) {
+            stack[i].decreasePrice(percent);
+        }
+    }
+
+    //private methods
+
+    //checks if the stack is full
+    private boolean isStackFull() {
+        return itemNumber==stack.length;
+    }
+
+    //checks if the stack is empty
+    private boolean isStackEmpty() {
+        return itemNumber==0;
+    }
 }
--- src/laba2/Item.java
+++ src/laba2/Item.java
@@ -1,2 +1,53 @@
-package laba2;public class Item {
+package laba2;
+
+public class Item {
+    //fields
+    private String name;        //item name
+    private float price;        //item price
+
+    //constructor
+    public Item (String name, float price) {
+        this.name=name;
+        this.price=price;
+        isPriceNegative();
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+    public float getPrice() {
+        return price;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name=name;
+    }
+    public void setPrice(float price) {
+        this.price=price;
+        isPriceNegative();
+    }
+
+    //methods
+
+    //increase in price by a certain percentage
+    public void decreasePrice(float percent) {
+        price-=price*percent/100;
+        isPriceNegative();
+    }
+
+    //decrease in price by a certain percentage
+    public void increasePrice(float percent) {
+        price+=price*percent/100;
+    }
+
+    //private methods
+
+    //price negativity test
+    private void isPriceNegative() {
+        if (price<0) {
+            price=0;
+        }
+    }
 }
